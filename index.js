2198abad1715c37ead7bc78df19616af7b3c37a5
Pavel <pavel@admin.ru>
2025-01-27 09:02:35 +0300

    prettier

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import _ from 'lodash'
+import _ from "lodash";
 
 console.log(_.intersection([2, 1], [2, 3])); // => [2]
+
+const nes = () => {
+  let paramas = [4, 43, 435, 1, 55];
+  for (const item of paramas) {
+    console.log(item);
+  }
+};
+nes();
